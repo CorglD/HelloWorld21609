33e2b0c128fca4f7c99e6fef9474219aaae8df5b
orglc <52209782@edu.campus02.at>
2022-09-16 19:13:15 +0200

    Initial Commit

---
+++ src/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+    }
+}
